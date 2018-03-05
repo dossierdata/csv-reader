05f897a12aa9466ca89c1df5620063ef0aed877b
VictorPreda <victor@dossierdata.nl>
2018-03-05 14:47:50 +0000

    Initial commit.

---
+++ CSVReaderServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DossierData\CsvReader;
+
+use Illuminate\Support\ServiceProvider;
+
+class CSVReaderServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
